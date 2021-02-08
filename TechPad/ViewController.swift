aaa67670f53b199ff3f3dc2eb7c7754eb07155b9
Nagiko Tatsui <n.t.woody.0914@gmail.com>
2021-02-08 10:35:44 +0900

    音楽プログラム途中

--- TechPad/ViewController.swift
+++ TechPad/ViewController.swift
@@ -6,10 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     //StoryBoardで扱うTableViewを宣言
     @IBOutlet var table: UITableView!
+    
+    //曲名を入れるための配列
+    var songNameArray = [String]()
+    //曲のファイル名を入れるための配列
+    var fileNameArray = [String]()
+    //音楽家の画像を入れるための配列
+    var imageNameArray = [String]()
+    //音楽を再生するための配列
+    var audioPlayer = AVAudioPlayer!
+    //
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,20 +28,50 @@ class ViewController: UIViewController {
         
         //テーブルビューのデータソースメソッドはViewControllクラスに書くよという設定
         table.dataSource = self
+        
+        //テーブルビューのデリケートメソッドはViewControllerメソッドに書くよという設定
+        table.delegate = self
+        
+        //songNameArrayに曲名を入れていく
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+        //fileNameArrayに曲名を入れていく
+        songNameArray = ["cannon", "elise", "aria"]
+        //imageNameArrayに曲の画像を入れていく
+        songNameArray = ["Pachelbel.jpg", "Beethoven.jpg", "Bach.jpg"]
     }
     
     //セルの数を設定
-    func tableView(_ tableView: UITableView, numberOfInSection section:Int) -> Int {
-        return 10
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        // セルの数をsongNameArrayの数にする
+        return songNameArray.count
     }
     
     //ID付きのセルを取得してセルフ属のtextLabelに「テスト」と表示させてみる
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath) -> UITableViewCell{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell?.textLabel?.text = "テスト"
+        //セルにsongNameArrayの曲名を表示する
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        //セルにsongNameArrayの曲名を表示する
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
         
         return cell!
+        
+    }
+    
+    //セルが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("\(songNameArray[indexPath.row])が選ばれました！")
+        
+        //音楽ファイルの設定
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource:fileNameArray[indexPath.row], ofType: "mp3")!)
+        
+        //再生の準備
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        
+        //音楽を再生
+        audioPlayer.play()
     }
 
 
